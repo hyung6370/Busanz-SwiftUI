3e5e43d113f1b2e1d42092336176b0b994d86c88
Hyungjun KIM <rlagudwns886@gmail.com>
2024-08-05 16:10:33 +0900

    Initial Commit

---
+++ Busanz-SwiftUI/Busanz_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Busanz_SwiftUIApp.swift
+//  Busanz-SwiftUI
+//
+//  Created by Hyungjun KIM on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Busanz_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
