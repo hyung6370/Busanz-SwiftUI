cd6719c7eb874501478a1d2f4b7fd0b3cf7a6808
Hyungjun KIM <rlagudwns886@gmail.com>
2024-08-23 15:25:21 +0900

    [Feat]: 맛집 상세 페이지에 네이버 맵 추가

---
+++ Busanz-SwiftUI/Present/NaverMapUI/NaverMapView.swift
@@ -0,0 +1,18 @@
+//
+//  NaverMapView.swift
+//  Busanz-SwiftUI
+//
+//  Created by Hyungjun KIM on 8/23/24.
+//
+
+import SwiftUI
+
+struct NaverMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NaverMapView()
+}
