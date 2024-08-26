05c1bfe59de7814783675f22db2f332d4705ccc1
Hyungjun KIM <rlagudwns886@gmail.com>
2024-08-26 14:55:49 +0900

    [Feat] : 맛집 상세정보 전화번호 통화 기능 추가

--- Busanz-SwiftUI/Present/DetailResInfoUI/DetailResInfoView.swift
+++ Busanz-SwiftUI/Present/DetailResInfoUI/DetailResInfoView.swift
@@ -10,34 +10,78 @@ import SwiftUI
 struct DetailResInfoView: View {
     var restaurant: Restaurant?
     
+    @State private var isExpanded: Bool = false
+    
     var body: some View {
-        VStack {
-            if let restaurant = restaurant {
-                ZStack {
-                    Rectangle()
-                        .fill(Color.customOrange)
-                        .cornerRadius(15)
-                        .frame(width: 370, height: 440)
+        ScrollView {
+            VStack(alignment: .leading) {
+                if let restaurant = restaurant {
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.customOrange)
+                            .cornerRadius(15)
+                            .frame(width: 360, height: 410)
+                        
+                        NaverMapView(lat: restaurant.lat, lng: restaurant.lng)
+                            .padding([.leading, .trailing], 20)
+                            .frame(width: 380, height: 390)
+                    }
                     
-                    NaverMapView(lat: restaurant.lat, lng: restaurant.lng)
-                        .padding([.leading, .trailing], 20)
-                        .frame(width: 380, height: 410)
+                    VStack(alignment: .leading, spacing: 3) {
+                        Text("주소")
+                            .font(.notosansBold16)
+                            .padding(.leading, 3)
+                        HStack(spacing: 4) {
+                            Image(systemName: "house.circle")
+                                .foregroundColor(.black)
+                                .font(.system(size: 16))
+                                .padding(.top, 3)
+                            Text("부산시 " + restaurant.addr1)
+                                .font(.notosansBold18)
+                        }
+                    }
+                    .padding([.leading, .trailing], 15)
+                    .padding(.top, 5)
+                    
+                    Text(restaurant.itemContent)
+                        .font(.notosansRegular16)
+                        .padding(.top, 2)
+                        .padding([.leading, .trailing], 15)
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        if restaurant.contactTel != "" {
+                            Text("연락처")
+                                .font(.notosansBold16)
+                                .padding(.leading, 3)
+                            HStack(spacing: 4) {
+                                Image(systemName: "phone.circle")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 16))
+                                Link("\(restaurant.contactTel)", destination: URL(string: "tel://\(restaurant.contactTel)")!)
+                                    .foregroundColor(.blue)
+                            }
+                        }
+                    }
+                    .padding([.leading, .trailing], 15)
+                    
+                    DisclosureGroup("운영 시간", isExpanded: $isExpanded) {
+                        Text(restaurant.usageDayWeekAndTime)
+                            .font(.notosansRegular12)
+                            .padding([.top, .bottom], 5)
+                            .padding([.leading, .trailing], 15)
+                    }
+                    .font(.notosansRegular16)
+                    .padding([.leading, .trailing], 25)
+                    .padding(.bottom, 10)
+                    
+                    Spacer()
                 }
-                
-                
-                Text(restaurant.addr1)
-                    .font(.notosansBold24)
-                    
-                Text(restaurant.itemContent)
-                    .padding()
-                
-                Spacer()
+                else {
+                    Text("맛집 정보를 불러올 수 없습니다.")
+                }
             }
-            else {
-                Text("맛집 정보를 불러올 수 없습니다.")
-            }
+            .navigationTitle(restaurant?.title ?? "상세 정보")
         }
-        .navigationTitle(restaurant?.title ?? "상세 정보")
     }
 }
 
