159c8e429d767434846b40d36a7911100a51934d
Hyungjun KIM <rlagudwns886@gmail.com>
2024-09-10 09:58:36 +0900

    [Add]: UITest 추가

---
+++ Busanz-SwiftUIUITests/Busanz_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Busanz_SwiftUIUITestsLaunchTests.swift
+//  Busanz-SwiftUIUITests
+//
+//  Created by Hyungjun KIM on 9/10/24.
+//
+
+import XCTest
+
+final class Busanz_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
